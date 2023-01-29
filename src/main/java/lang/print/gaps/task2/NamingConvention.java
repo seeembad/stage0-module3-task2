8a12ebb5c8295f75736682aafe709cd21af39d0a
moseyarthas <moseyarthas@gmail.com>
2023-01-30 00:00:35 +0300

    refactor task

--- src/main/java/lang/print/gaps/task2/NamingConvention.java
+++ src/main/java/lang/print/gaps/task2/NamingConvention.java
@@ -3,7 +3,7 @@ package lang.print.gaps.task2;
 public class NamingConvention {
     public static void main(String[] args) {
         int variable = 10;
-        int variable_ne = 1;
+        int variable_one = 1;
         int variableTwo = 2;
         int variable_three = 3;
         final int VARIABLE_FOUR = 4;
